f669fb7b8f905b2eff1cc34bbba9bbcf4c21e108
Sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-01-04 21:52:31 +0530

    Add files via upload

---
+++ rock-paper-scissor.js
@@ -0,0 +1,100 @@
+
+let score = JSON.parse(localStorage.getItem('score')) || {
+    Win: 0,
+    Lose: 0,
+    Tie: 0
+}
+updateScore();
+
+let resultOnPage=document.querySelector('#result-onPage');
+
+
+function playGame(myMove) {
+    let result;
+    let computerMove = PickcomputerMove();
+    
+    if (myMove === 'Rock') {
+        if (computerMove === 'Rock') {
+            result = 'Tie'
+        }
+        else if (computerMove === 'Scissors') {
+            result = 'Win'
+        }
+        else {
+            result = 'Lose'
+        }
+    }
+
+    else if (myMove === 'Scissors') {
+        if (computerMove === 'Rock') {
+            result = 'Lose'
+        }
+        else if (computerMove === 'Scissors') {
+            result = 'Tie'
+        }
+        else {
+            result = 'Win'
+        }
+    }
+
+    else {
+        if (computerMove === 'Rock') {
+            result = 'Win'
+        }
+        else if (computerMove === 'Scissors') {
+            result = 'Lose'
+        }
+        else {
+            result = 'Tie'
+        }
+    }
+    getScore(result);
+     below.innerHTML=`<div class='small-box'> Your move : <img class='small-image' src="img/${myMove}.jpg"><br><br><img class='small-image' src="img/${computerMove}.jpg"> : Computer Move <div>`;
+
+    resultOnPage.innerText=`${result}\n`;
+
+    document.querySelector('#score').innerText=`\n\nScore :\nWin - ${score.Win}\nLose - ${score.Lose}\nTie - ${score.Tie}`;
+
+    localStorage.setItem('score', (JSON.stringify(score)));
+}
+
+function PickcomputerMove() {
+    let computerMove;
+
+    let randNum = Math.random();
+    if (randNum >= 0 && randNum < 1 / 3) {
+        computerMove = 'Rock';
+    }
+    else if (randNum >= 1 / 3 && randNum < 2 / 3) {
+        computerMove = 'Paper';
+    }
+    else {
+        computerMove = 'Scissors';
+    }
+
+    return computerMove;
+
+}
+
+function getScore(result) {
+
+    if (result === 'Win') {
+        score.Win++;
+    }
+
+    else if (result === 'Lose') {
+        score.Lose++;
+    }
+
+    else {
+        score.Tie++;
+    }
+
+
+}
+
+function updateScore()
+{
+    document.querySelector('#score').innerHTML=`<br><u><spam style='color:white;font-size:30px;font-weight:bold;padding-bottom:20px;'>Score :</u></spam><br><br>Win - ${score.Win}<br>Lose - ${score.Lose}<br>Tie - ${score.Tie}`;
+}
+
